8e33703f2196c0ad8246f690109e0834ba4f82f2
MarkGee45 <moel55.mg@gmail.com>
2021-03-21 23:57:22 -0700

    work on addition

--- js/hw2_p2.js
+++ js/hw2_p2.js
@@ -1,17 +1,25 @@
+let n1, n2, n3, num1, num2, num3;
 
-let results = document.getElementById('result').value;
-
+// function input() {
+    
+//     // ((typeof n1 || n2 || n3 !== 'number') || (n1 || m2 || n3 === ''))? 
+//     // document.getElementById('result').value = "Your input is not a number.":
+   
+// }
 
 function threeNumbers() {
-    let n1 = document.getElementById('num-one').value;
-    console.log(n1)
-    let n2 = document.getElementById('num-two').value;
-    let n3 = document.getElementById('num-three').value;
-    let num1 = parseInt(n1);
-    let num2 = parseInt(n2);
-    let num3 = parseInt(n3);
+    n1 = document.getElementById('num-one').value;
+    n2 = document.getElementById('num-two').value;
+    n3 = document.getElementById('num-three').value;
+
+    num1 = parseInt(n1);
+    num2 = parseInt(n2);
+    num3 = parseInt(n3);
+
+    console.log(num1, num2, num3)
     let sum = num1 + num2 + num3;
     let average = sum / 3;
     let product = num1 * num2 * num3;
-    results = "The sum is" + sum;
+   
+    document.getElementById('result').value = ("The sum is " + sum + "\nThe average is " + average + "\nThe product is " + product);
 }
